6dfe6133ece21308e58e92a76bebafd06b9a760b
nakashita-lvgs <takuya.nakashita@leverages.jp>
2024-03-04 18:49:41 +0900

    vava navi

--- src/domain/auction.test.ts
+++ src/domain/auction.test.ts
@@ -1,27 +1,35 @@
 import { describe } from "node:test";
 import { Auction } from "./auction";
 
+beforeEach(() => {
+  const now = new Date("2026-01-01T00:00:00Z");
+  jest.useFakeTimers().setSystemTime(now.getTime());
+});
 afterEach(() => {
   jest.useRealTimers();
 });
 
 describe("Auction", () => {
   test("初期化できる", () => {
-    const startAt = new Date("2025-01-01T00:00:00Z");
+    const startAt = new Date("2027-01-01T00:00:00Z");
 
-    const auction = Auction.of(startAt);;
+    const auction = Auction.of(startAt);
     expect(auction).toBeInstanceOf(Auction);
   });
   test("開始時刻が過去の場合は、オークションは作成できない", () => {
-    const startAt = new Date("2021-01-01T00:00:00Z");
-    
+    const startAt = new Date("2025-12-31T23:59:59Z");
+
     expect(() => {
       Auction.of(startAt);
     }).toThrow("開始時刻が過去です");
   });
-  // test("終了時刻が開始時刻より過去の場合は、オークションは作成できない", () => {
-  //   fail();
-  // });
+  test("終了時刻が開始時刻より過去の場合は、オークションは作成できない", () => {
+    const startAt = new Date("2027-01-01T00:00:00Z");
+    const endAt = new Date("2026-12-31T23:59:59Z");
+    expect(() => {
+      Auction.of(startAt, endAt);
+    }).toThrow("終了時刻が開始時刻より過去です");
+  });
   // test("オークションを開始する", () => {
   //   fail();
   // });
--- src/domain/auction.ts
+++ src/domain/auction.ts
@@ -1,13 +1,16 @@
 export class Auction {
-    constructor(
-        readonly startAt: Date,
-    ) {
-    }
+  constructor(
+    readonly startAt: Date,
+    readonly endAt: Date,
+  ) {}
 
-    static of(startAt: Date): Auction {
-        if (startAt < new Date()) {
-            throw new Error("開始時刻が過去です");
-        }
-        return new Auction(startAt);
+  static of(startAt: Date): Auction {
+    if (startAt < new Date()) {
+      throw new Error("開始時刻が過去です");
     }
+    if (startAt > endAt) {
+      throw new Error("終了時刻が開始時刻より過去です");
+    }
+    return new Auction(startAt);
+  }
 }
