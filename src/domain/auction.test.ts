342e88a241216f40752b2afef1cc8b7fc176a114
maehara-lvgs <sotaro.maehara@leverages.jp>
2024-04-22 18:40:44 +0900

    maehara

--- src/domain/auction.test.ts
+++ src/domain/auction.test.ts
@@ -7,12 +7,12 @@ afterEach(() => {
 
 describe("Auction", () => {
   test("初期化できる", () => {
-    const auction = new Auction();
+    const auction = new Auction(new Date("2024-04-22"));
     expect(auction).toBeDefined();
   });
   test("開始時刻が過去の場合は、オークションは作成できない", () => {
-    const auction = new Auction({started_at: new Date("2024-04-22")});
-    expect(auction.started_at).toBe(new Date("2024-04-22")) 
+    const auction = new Auction(new Date("2024-04-22"));
+    expect(auction.started_at).toThrow();
   });
   // test("終了時刻が開始時刻より過去の場合は、オークションは作成できない", () => {
   //   fail();
